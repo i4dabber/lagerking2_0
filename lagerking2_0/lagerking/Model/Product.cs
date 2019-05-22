39ae54f56025e54b6730cf88a828702f1fe93580
Mike Birch <mike-mj@hotmail.com>
2019-05-22 14:50:19 +0200

    Database setup

---
+++ lagerking2_0/lagerking/Model/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace lagerking
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(25)]
+        public string Name { get; set; }
+
+        [Required, StringLength(40)]
+        public string Description { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Stock must be atleast: 0!")]
+        public int Stock { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Price must be greater than: 0!")]
+        public int Price { get; set; }
+
+        //public List<Cart.cart> Carts { get; set; }
+    }
+}
